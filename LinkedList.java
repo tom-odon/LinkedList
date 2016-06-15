f9146a777fae3814da3da5e31a6f4e654492d0e0
Tom <thomas.odon@gmail.com>
2016-06-15 14:23:44 -0500

    Added additional add/remove methods, updated Git email

--- LinkedList.java
+++ LinkedList.java
@@ -23,13 +23,12 @@ public class LinkedList {
 	public LinkedList(){
 		first = null;
 		N = 0;
-
 	}
 
 	/*
 	*  Insert a new node at the beginning. 
 	*/
-	public void insert(Item item) {
+	public void insertAtStart(Item item) {
 		Node oldFirst = first;      //we save a link to the current list
 		first = new Node(); 		//first is class field and now holds a new node
 		first.item = item;			//set our new nodes' item to the method arg
@@ -37,18 +36,56 @@ public class LinkedList {
 		N++;		
 	}
 
+	public void removeFromStart() {
+		this.first = first.next;
+	}
+
+	/*
+	* Insert a new node at the end.
+	*/
+	public void insertAtEnd(Item item) {
+		Node end = new Node();		//create a new node to add at end
+		end.item = item;			//add the arg into new node's data
+		Node n = this.first;		//iterate through each node starting at top
+		while(n.next != null)		//until we encounter the last node with no
+			n = n.next;				//'next' linked to it.
+		n.next = end;				//Link the end node.
+	}
+
+
+
 	public static void main(String[] args) {
 
 		LinkedList list = new LinkedList();
 
-		list.insert(new Item("spain"));
-		list.insert(new Item("in"));	
-		list.insert(new Item("rain"));
-		list.insert(new Item("the"));
+		list.insertAtStart(new Item("spain"));
+		list.insertAtStart(new Item("in"));	
+		list.insertAtStart(new Item("rain"));
+		list.insertAtStart(new Item("the"));
+
+		System.out.println("\n Test insert at start: \n");
 
 		for (Node x = list.first; x != null; x = x.next) {
 			System.out.println(x.item);
-		}				
+		}
+
+		System.out.println("\n Test remove from start: \n");
+
+		list.removeFromStart();
+
+		for (Node x = list.first; x != null; x = x.next) {
+			System.out.println(x.item);
+		}
+
+		System.out.println("\n Test insert at end: \n");
+
+		list.insertAtEnd(new Item("stains"));
+		list.insertAtEnd(new Item("my"));
+		list.insertAtEnd(new Item("brain"));
+
+		for (Node x = list.first; x != null; x = x.next) {
+			System.out.println(x.item);
+		}
 
 	}
 
